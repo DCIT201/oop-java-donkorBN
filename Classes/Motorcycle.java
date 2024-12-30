e28036432d4636ef4948a3320eab6361d8462627
donkorBN <projectmanuel89@gmail.com>
2024-12-30 21:10:23 +0000

    added Motorcycle class

---
+++ Classes/Motorcycle.java
@@ -0,0 +1,36 @@
+public class Motorcycle extends Vehicle implements Rentable {
+
+    public Motorcycle(String vehicleId, String model, double baseRentalRate) {
+        super(vehicleId, model, baseRentalRate);
+    }
+
+    @Override
+    public double calculateRentalCost(int days) {
+        return getBaseRentalRate() * days;
+    }
+
+    @Override
+    public boolean isAvailableForRental() {
+        return isAvailable();
+    }
+
+    @Override
+    public void rent(Customer customer, int days) {
+        if (!isAvailableForRental()) {
+            System.out.println("The motorcycle is currently unavailable for rental.");
+            return;
+        }
+        setAvailable(false);
+        System.out.println("Rental confirmed: " + customer.getName() + " has rented the motorcycle for " + days + " days.");
+    }
+
+    @Override
+    public void returnVehicle() {
+        if (isAvailable()) {
+            System.out.println("The motorcycle is already marked as available.");
+            return;
+        }
+        setAvailable(true);
+        System.out.println("The motorcycle has been successfully returned and is now available for rental.");
+    }
+}
